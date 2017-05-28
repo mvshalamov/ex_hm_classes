736eb4882d2d409cb2bf8ffa5d5d98b879b18bd3
mvshalamov <mvshalamov@sberned.ru>
2017-05-28 10:33:48 +0300

    dict homework

---
+++ tests/test_mydict.py
@@ -0,0 +1,11 @@
+import pytest
+
+from ex_hm_classes.hw.ex4 import MyDict
+
+
+def test_init():
+	mydict = MyDict([('one', 1), ('two', 2), ('three', 3)])
+	assert mydict.one == 1
+
+	with pytest.raises(AttributeError):
+		mydict.test == 1
