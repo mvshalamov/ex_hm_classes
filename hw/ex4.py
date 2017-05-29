fa4ef42f2b374ed5c39f456e3bc828a9e3888c4b
mvshalamov <mvshalamov@domclick.ru>
2017-05-29 13:17:35 +0300

    clean

--- hw/ex4.py
+++ hw/ex4.py
@@ -1,22 +1,22 @@
 class MyDict(dict):
-        """
-        >>> x = MyDict([('one', 1), ('two', 2), ('three', 3)])
-        >>> print(x['three'])
-        3
-        >>> print(x.get('two'))
-        2
-        >>> print(x.one)
-        1
-        >>> print(x.test)
-        Traceback (most recent call last):
-        ...
-        AttributeError
-        """
-        def __getattr__(self, name):
-            try:
-                return self[name]
-            except KeyError:
-                raise AttributeError
+    """
+    >>> x = MyDict([('one', 1), ('two', 2), ('three', 3)])
+    >>> print(x['three'])
+    3
+    >>> print(x.get('two'))
+    2
+    >>> print(x.one)
+    1
+    >>> print(x.test)
+    Traceback (most recent call last):
+    ...
+    AttributeError
+    """
+    def __getattr__(self, name):
+        try:
+            return self[name]
+        except KeyError:
+            raise AttributeError
 
 
 if __name__ == "__main__":
