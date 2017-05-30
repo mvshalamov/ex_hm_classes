989ff2d19c63b7e44442cbabe9fc62082d1a2ca5
mvshalamov <mvshalamov@domclick.ru>
2017-05-30 10:06:09 +0300

    clean

--- tests/test_mydict.py
+++ tests/test_mydict.py
@@ -4,8 +4,8 @@ from ex_hm_classes.hw.ex4 import MyDict
 
 
 def test_init():
-	mydict = MyDict([('one', 1), ('two', 2), ('three', 3)])
-	assert mydict.one == 1
+    mydict = MyDict([('one', 1), ('two', 2), ('three', 3)])
+    assert mydict.one == 1
 
-	with pytest.raises(AttributeError):
-		mydict.test == 1
+    with pytest.raises(AttributeError):
+        mydict.test == 1
